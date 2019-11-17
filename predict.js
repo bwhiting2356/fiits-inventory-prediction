a0ae85cb62030f2ccd2675f6e90fad9bf3190647
bwhiting2356 <brendan.whiting@me.com>
2019-11-16 21:07:06 -0800

    write file from env var

--- predict.js
+++ predict.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+fs.writeFileSync('key.json', process.env.GOOGLE_CREDENTIALS_JSON);
+
 const automl = require('@google-cloud/automl');
 
 const client = new automl.PredictionServiceClient();
